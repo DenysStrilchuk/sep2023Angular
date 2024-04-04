770ab6ed3a71fc4df0d86927e4b68ffb39e558b0
Denys Sttrilchuk <denia876@gmail.com>
2024-04-04 20:34:23 +0300

    lesson-5 part-1(error 401)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {UsersModule} from "./modules/users/users.module";
-import {PostsModule} from "./modules/posts/posts.module";
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { HeaderComponent } from './components/header/header.component';
 
---
+++ src/app/modules/cars/components/cars/cars.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {CarService} from "../../../../services/car.service";
+import {ICar} from "../../../../interfaces/car.interface";
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrl: './cars.component.css'
+})
+export class CarsComponent implements OnInit {
+  cars: ICar[]
+
+  constructor(private carService: CarService) {
+  }
+
+  ngOnInit(): void {
+    this.carService.getAll().subscribe(value => this.cars = value)
+  }
+}
